c7d4b09aebc5d665d2902b642fc40cb9556a384e
Andrei Visan <andrei.m.visan@gmail.com>
2021-04-14 09:22:36 +0200

    LinkedList Algorithms - Linked List Cycle II - Partial commit - Java

---
+++ datastructures/linkedlist/java/algorithms/LinkedListCycleII.java
@@ -0,0 +1,36 @@
+package datastructures.linkedlist.java.algorithms;
+
+public class LinkedListCycleII {
+
+    public static ListNode detectCycle(ListNode head) {
+        ListNode tortoise = head;
+        ListNode hare = head;
+
+        // A fast pointer will either loop around a cycle and meet the slow
+        // pointer or reach the `null` at the end of a non-cyclic list.
+        while (hare != null && hare.next != null) {
+            tortoise = tortoise.next;
+            hare = hare.next.next;
+            if (tortoise == hare) {
+                return tortoise;
+            }
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node2;
+
+        System.out.println(detectCycle(node1).val);
+    }
+
+}
