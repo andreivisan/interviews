d06061f60abc6525c691253eba86ed0eac0a68a8
Andrei Visan <andrei.visan@kpn.com>
2021-04-30 17:59:59 +0200

    Doubly Linked List - Finished implementation - Java

--- datastructures/linkedlist/java/impl/DoublyLinkedList.java
+++ datastructures/linkedlist/java/impl/DoublyLinkedList.java
@@ -7,6 +7,24 @@ public class DoublyLinkedList<T> {
         head = null;
     }
 
+    public T get(int index) {
+        if (index > size) {
+            return null;
+        }
+
+        Node<T> current = head;
+
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+
+        if (current != null) {
+            return current.data;
+        } else {
+            return null;
+        }
+    }
+
     public void addAtHead(T val) {
         if (head == null) {
             head = new Node<T>(val);
@@ -20,6 +38,77 @@ public class DoublyLinkedList<T> {
         }
 
         size++;
+    }
+
+    public void addAtTail(T val) {
+        if (head == null) {
+            head = new Node<T>(val);
+        } else {
+            Node<T> current = head;
+            Node<T> newNode = new Node<T>(val);
+
+            while (current.next != null) {
+                current = current.next;
+            }
+
+            current.next = newNode;
+            newNode.previous = current;
+        }
+
+        size++;
+    }
+
+    public void addAtIndex(int index, T val) {
+        if (head == null) {
+            head = new Node<T>(val);
+        } else {
+            if (index == 0) {
+                addAtHead(val);
+            } else if (index >= size) {
+                addAtTail(val);
+            } else {
+                Node<T> current = head;
+                Node<T> newNode = new Node<T>(val);
+
+                for (int i = 0; i < index - 1; i++) {
+                    current = current.next;
+                }
+
+                Node<T> next = current.next;
+                newNode.previous = current;
+                newNode.next = next;
+                current.next = newNode;
+                next.previous = newNode;
+
+                size++;
+            }
+        }
+    }
+
+    public void deleteAtIndex(int index) {
+        if (head == null || index >= size) {
+            return;
+        }
+
+        if (index == 0) {
+            head = head.next;
+            size--;
+            return;
+        }
+
+        Node<T> current = head;
+
+        for (int i = 0; i < index - 1; i++) {
+            current = current.next;
+        }
+
+        Node<T> toDelete = current.next;
+        current.next = toDelete.next;
+        if (toDelete.next != null) {
+            toDelete.next.previous = current;
+        }
+
+        size--;
     }
 
     public int size() {
--- datastructures/linkedlist/java/test/DoublyLinkedListTest.java
+++ datastructures/linkedlist/java/test/DoublyLinkedListTest.java
@@ -6,8 +6,17 @@ public class DoublyLinkedListTest {
         dLinkedList.addAtHead(2);
         dLinkedList.addAtHead(1);
         dLinkedList.addAtHead(0);
+        dLinkedList.addAtTail(3);
+        dLinkedList.addAtIndex(0, -1);
+        dLinkedList.addAtIndex(5, 6);
+        dLinkedList.addAtIndex(5, 5);
+
+        dLinkedList.deleteAtIndex(6);
 
         dLinkedList.print();
+
+        System.out.println("\nValue at index 2 is: " + dLinkedList.get(2));
+        System.out.println("List size is: " + dLinkedList.size());
     }
 
 }
