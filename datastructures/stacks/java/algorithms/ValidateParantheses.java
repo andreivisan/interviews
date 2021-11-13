ed01521465f50a84ea1ecdca458e33ba26638f15
Andrei Visan <andrei.visan@kpn.com>
2021-11-13 14:15:49 +0100

    Stacks - Valid Parentheses - Java

---
+++ datastructures/stacks/java/algorithms/ValidateParantheses.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class ValidateParantheses {
+
+    public static boolean isValid(String s) {
+        if (s.length() < 2) {
+            return false;
+        }
+
+        Stack<String> temp = new Stack<>();
+
+        Map<String, String> paranthesesDict = new HashMap<>();
+        paranthesesDict.put("{", "}");
+        paranthesesDict.put("[", "]");
+        paranthesesDict.put("(", ")");
+
+        for (int i = 0; i < s.length(); i++) {
+            if (!temp.empty() && paranthesesDict.getOrDefault(temp.peek(), "*").equals(String.valueOf(s.charAt(i)))) {
+                temp.pop();
+            } else {
+                temp.push(String.valueOf(s.charAt(i)));
+            }
+        }
+
+        return temp.size() == 0;
+
+    }
+
+    public static void main(String[] args) {
+        String input = "([)]";
+
+        System.out.println("Is input valid? " + isValid(input));
+    }
+
+}
