5ede1c9e60bc4c2b1a82901512b03055b280c146
andreivisan <andrei.m.visan@gmail.com>
2022-01-23 15:10:07 +0100

    Completed Python LinkedList implementation

--- datastructures/linkedlist/python/linkedlist.py
+++ datastructures/linkedlist/python/linkedlist.py
@@ -1,3 +1,6 @@
+from email import header
+
+
 class Node:
     def __init__(self, value):
         self.value = value
@@ -15,9 +18,24 @@ class LinkedList:
     def is_empty(self):
         return self.size == 0
 
-    def add(self, value):
-        self.append(value)
-
+    def add(self, index, value):
+        if index > self.size:
+            self.append(value)
+            return
+        if index == 0 or self.is_empty():
+            self.add_first(value)
+            return
+        iterator = 0
+        current = self.head
+        while iterator < index - 1:
+            current = current.next
+            iterator += 1
+        new_node = Node(value)
+        next_node = current.next
+        current.next = new_node
+        new_node.next = next_node
+        self.size += 1
+        
     def append(self, value):
         if self.is_empty():
             self.head = Node(value)
@@ -38,6 +56,27 @@ class LinkedList:
         new_node.next = self.head
         self.head = new_node
         self.size += 1
+        
+    def delete(self, value):
+        if self.is_empty():
+            print("List is empty")
+            return
+        current = self.head
+        
+        if current and current.value == value:
+            self.head = current.next
+            return
+        
+        prev = current
+        while current and current.value != value:
+            prev = current
+            current = current.next
+            
+        if not current:
+            print("Value was not found!")
+            return
+        prev.next = current.next
+        self.size -= 1
 
     def print(self):
         current = self.head
@@ -49,8 +88,12 @@ class LinkedList:
 if __name__ == "__main__":
     linked_list = LinkedList()
 
-    linked_list.add(1)
-    linked_list.add(2)
+    linked_list.append(1)
+    linked_list.append(3)
     linked_list.add_first(0)
+    
+    linked_list.add(2, 2)
+    
+    linked_list.delete(4)
 
     linked_list.print()
